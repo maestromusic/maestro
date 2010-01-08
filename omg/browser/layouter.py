515756d6c3a0d2b7edef36a6754382034b0e1f24
Martin Altmayer <martin.altmayer@web.de>
2010-01-08 23:51:41 +0100

    Some improvements on ComplexLayouter.

--- omg/browser/layouter.py
+++ omg/browser/layouter.py
@@ -17,9 +17,9 @@ def _initTags():
     _artistTags = [tags.get(tagname) for tagname in config.get("browser","artist_tags").split(',')]
     
 _stdCharFormat = QtGui.QTextCharFormat()
-_stdCharFormat.setFontPointSize(10)
+_stdCharFormat.setFontPointSize(11)
 _titleCharFormat = QtGui.QTextCharFormat()
-_titleCharFormat.setFontPointSize(10)
+_titleCharFormat.setFontPointSize(11)
 _titleCharFormat.setFontWeight(QtGui.QFont.Bold)
 
 
@@ -34,20 +34,23 @@ class ComplexLayouter:
         
         cursor.blockFormat().setNonBreakableLines(True) # Don't break lines)
         
-        cursor.insertText(element.getTitle())
-        #~ # Construct the artist line
-        #~ artistList = []
-        #~ for tag in _artistTags:
-            #~ artistList.extend([artist for artist in element.tags[tag]])
-        #~ 
-        #~ if artistList:
-            #~ cursor.insertText(", ".join(artistList),_stdCharFormat)
-            #~ cursor.insertBlock()
-        #~ 
-        #~ if isinstance(element,browsermodels.ContainerNode):
-            #~ cursor.insertText(element.getTitle(),_titleCharFormat)
-        #~ else: cursor.insertText(element.getTitle(),_stdCharFormat)
-        #~ 
+        # Construct the artist line
+        artistList = []
+        for tag in _artistTags:
+            if tag in element.tags:
+                artistList.extend(element.tags[tag])
+        
+        if artistList:
+            cursor.insertText(", ".join(artistList),_stdCharFormat)
+            cursor.insertBlock()
+        
+        if element.isFile():
+            cursor.insertText(element.getTitle(),_stdCharFormat)
+        elif tags.DATE in element.tags:
+            dateString = "/".join(str(date.year) for date in element.tags[tags.DATE]) # Support multiple date tags
+            cursor.insertText("{0} - {1}".format(dateString,element.getTitle()),_titleCharFormat)
+        else: cursor.insertText(element.getTitle(),_titleCharFormat)
+        
         #~ if isinstance(element,browsermodels.FileNode) and element.tags[tags.ALBUM]:
             #~ cursor.insertBlock()
             #~ cursor.insertText(element.tags[tags.ALBUM][0],_stdCharFormat)
