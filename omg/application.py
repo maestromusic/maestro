ebff0517c467fc6ce4b81d9db4eabeb02d432a3a
Martin Altmayer <martin.altmayer@web.de>
2010-04-27 18:07:34 +0200

    It's only rudimentary...BUT IT PLAYS MUSIC

--- omg/application.py
+++ omg/application.py
@@ -9,26 +9,39 @@
 import sys
 from PyQt4 import QtCore, QtGui
 
-# Import and initialize modules
-from omg import database
-database.connect()
-from omg import tags
-tags.updateIndexedTags()
-from omg import config, browser, search
-search.init()
+def run():
+    # Some Qt-classes need a running QApplication before they can be created
+    app = QtGui.QApplication(sys.argv)
 
-# Create GUI
-app = QtGui.QApplication(sys.argv)
-gui = QtGui.QWidget()
-layout = QtGui.QHBoxLayout()
-gui.setLayout(layout)
+    # Import and initialize modules
+    from omg import database
+    database.connect()
+    from omg import tags
+    tags.updateIndexedTags()
+    from omg import config, mpclient, search, browser, playlist
+    search.init()
 
-browser = browser.Browser(None)
-layout.addWidget(browser)
+    # Create GUI
+    gui = QtGui.QWidget()
+    layout = QtGui.QHBoxLayout()
+    gui.setLayout(layout)
 
-gui.resize(600, 410)
-screen = QtGui.QDesktopWidget().screenGeometry()
-size =  gui.geometry()
-gui.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
-gui.show()
-sys.exit(app.exec_())
+    browser = browser.Browser(gui)
+    layout.addWidget(browser,2)
+
+    playlist = playlist.PlayList(gui)
+    layout.addWidget(playlist,3)
+
+    browser.nodeDoubleClicked.connect(playlist.addContainer)
+
+
+    gui.resize(600, 410)
+    screen = QtGui.QDesktopWidget().screenGeometry()
+    size =  gui.geometry()
+    gui.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
+    gui.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    run()
