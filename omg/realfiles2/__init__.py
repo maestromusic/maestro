c5c776b9f8603c8ec268028605a8a644606810f5
Michael Helmling <michaelhelmling@posteo.de>
2011-08-06 15:22:13 +0200

    removed debug stuff

--- omg/realfiles2/__init__.py
+++ omg/realfiles2/__init__.py
@@ -11,7 +11,6 @@ from .. import tags, logging
 from ..utils import absPath
 from ..config import options
 import cutags
-cutags.setVerbosity(100)
 
 logger = logging.getLogger("realfiles2")
 
@@ -92,7 +91,7 @@ class UFile(RealFile):
     def _ensureFileIsLoaded(self):
         if self._f is None:
             self._f = cutags.File(self.path)
-            print(self._f.tags)
+
     def read(self):
         self._ensureFileIsLoaded()
         self.tags = tags.Storage()
