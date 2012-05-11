bf5a86f5def21237dd35794ac1e8f3ff09511030
Michael Helmling <michaelhelmling@posteo.de>
2012-05-11 22:16:47 +0200

    initial renamer plugin

---
+++ omg/plugins/renamer/__init__.py
@@ -0,0 +1,4 @@
+NAME="Renamer"
+AUTHOR="Michael Helmling"
+VERSION="0.0alpha"
+DESCRIPTION="Rename files based on container and tag structure using a flexible specification language"
---
+++ omg/plugins/renamer/plugin.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# OMG Music Manager  -  http://omg.mathematik.uni-kl.de
+# Copyright (C) 2009-2012 Martin Altmayer, Michael Helmling
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from omg.core import tags
+from omg import logging
+
+logger = logging.getLogger(__name__)
+
+def defaultStorage():
+    return {"SECTION:renamer":
+            {'profiles': ("default", "<artist>") } }
+
+def enable():
+    pass
+    
+
+import pyparsing
+from pyparsing import Literal, Word, alphas, alphanums
+
+lbrace = Literal("<").suppress()
+rbrace = Literal(">").suppress()
+
+tagName = Word(alphas + "_", alphanums + "_:()/\\")
+print('wtf')
+#print(tagName.parseString("<zomg"))
+logger.info("renamer enabled")
+        
+
+def disable():
+    pass
