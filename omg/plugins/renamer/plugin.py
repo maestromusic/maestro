b5e7614856ec30364142f46d0926453a70ae16ee
Michael Helmling <michaelhelmling@posteo.de>
2013-01-06 00:27:43 +0100

    Change default format string in the renamer

--- omg/plugins/renamer/plugin.py
+++ omg/plugins/renamer/plugin.py
@@ -126,7 +126,7 @@ class GrammarRenamer(profiles.Profile):
             state = {}
         if 'formatString' in state:
             self.formatString = state['formatString']
-        else: self.formatString = "<artist>/I AM A DEFAULT FORMAT STRING/<1.title>/<#> - <title>"
+        else: self.formatString = "<1.artist>/<1.title>/<#> - <title>"
         if 'replaceChars' in state:
             self.replaceChars = state['replaceChars']
         else: self.replaceChars = '\\:/'
