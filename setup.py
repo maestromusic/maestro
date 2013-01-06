3cef20bb0d72e7125eb4fc45b981b6578f2d65ed
Michael Helmling <michaelhelmling@posteo.de>
2013-01-06 21:01:16 +0100

    Fix and update setup.py; remove unneccessary MANIFEST.im

--- setup.py
+++ setup.py
@@ -20,88 +20,89 @@
 import sys
 
 if sys.version_info.major <= 2:
-  print("OMG runs with Python 3.x only. "
-        "Please re-run setup.py with a Python3 interpreter.")
-  sys.exit(1)
+    print("OMG runs with Python 3.x only. "
+          "Please re-run setup.py with a Python3 interpreter.")
+    sys.exit(1)
 
 import distribute_setup
 
 distribute_setup.use_setuptools()
 
-import glob, os, subprocess
-from os.path import getmtime
+import glob, os.path, subprocess
+from os.path import dirname, exists, join, getmtime
 
 from setuptools import setup, find_packages
 from setuptools.command import sdist, install, develop, test
 
-def updateTranslations():
-    """Create *.qm files from *.ts in i18n folder if necessary."""
-    tsFiles = glob.glob("i18n/*.ts")
-    proFile = "i18n/omg.pro"
-    for tsFile in tsFiles:
-        qmFile = tsFile[:-2] + "qm"
-        if not os.path.exists(qmFile) or getmtime(tsFile) > getmtime(qmFile):
-            print("Updating translation file: {}".format(qmFile))
-            try:
-                subprocess.check_call(["lrelease", proFile])
-            except Exception as e:
-                print(e)
+if exists(join(dirname(__file__), '.git')):
+    def updateTranslations():
+        """Create *.qm files from *.ts in i18n folder if necessary."""
+        tsFiles = glob.glob("i18n/*.ts")
+        proFile = "i18n/omg.pro"
+        for tsFile in tsFiles:
+            qmFile = tsFile[:-2] + "qm"
+            if not os.path.exists(qmFile) or getmtime(tsFile) > getmtime(qmFile):
+                print("Updating translation file: {}".format(qmFile))
                 try:
-                    subprocess.check_call(["lrelease-qt4", proFile])
+                    subprocess.check_call(["lrelease", proFile])
                 except Exception as e:
                     print(e)
-                    print("Warning: Could not update translations")
-                    return
+                    try:
+                        subprocess.check_call(["lrelease-qt4", proFile])
+                    except Exception as e:
+                        print(e)
+                        print("Warning: Could not update translations")
+                        return
+        
     
-
-def updateResources():
-    """Update resource files with pyrcc4 if necessary."""
-    resources = [ (["images/images.qrc"], "omg/resources.py"),
-                  (["i18n/translations.qrc"] + glob.glob("i18n/*.qm"), "omg/translations.py") ]
-    pluginBaseDir = os.path.join("omg", "plugins")
-    for subdir in os.listdir(pluginBaseDir):
-        pluginDir = os.path.join(pluginBaseDir, subdir)
-        if os.path.exists(os.path.join(pluginDir, "resources.qrc")):
-            resources.append( ([os.path.join(pluginDir, "resources.qrc")],
-                               os.path.join(pluginDir, "resources.py")) )
-    for sources, py in resources:
-        for source in sources:
-            if not os.path.exists(py) or getmtime(source) > getmtime(py):
-                print("Updating resource file: {}".format(py))
-                subprocess.check_call(["pyrcc4", "-py3", "-o", py, sources[0]])
-                break
-
-# Now we monkey-path the relevant commands to update i18n & resources on demand
-sdist_run = sdist.sdist.run
-def wrapped_sdist_run(self):
-    updateTranslations()
-    updateResources()
-    sdist_run(self)
-sdist.sdist.run = wrapped_sdist_run
-
-install_run = install.install.run
-def wrapped_install_run(self):
-    updateTranslations()
-    updateResources()
-    install_run(self) 
-install.install.run = wrapped_install_run
-
-develop_run = develop.develop.run
-def wrapped_develop_run(self):
-    updateTranslations()
-    updateResources()
-    develop_run(self)
-develop.develop.run = wrapped_develop_run
-
-test_run = test.test.run
-def wrapped_test_run(self):
-    updateTranslations()
-    updateResources()
-    test_run(self)
-test.test.run = wrapped_test_run
+    def updateResources():
+        """Update resource files with pyrcc4 if necessary."""
+        resources = [ (["images/images.qrc"], "omg/resources.py"),
+                      (["i18n/translations.qrc"] + glob.glob("i18n/*.qm"), "omg/translations.py") ]
+        pluginBaseDir = os.path.join("omg", "plugins")
+        for subdir in os.listdir(pluginBaseDir):
+            pluginDir = os.path.join(pluginBaseDir, subdir)
+            if os.path.exists(os.path.join(pluginDir, "resources.qrc")):
+                resources.append( ([os.path.join(pluginDir, "resources.qrc")],
+                                   os.path.join(pluginDir, "resources.py")) )
+        for sources, py in resources:
+            for source in sources:
+                if not os.path.exists(py) or getmtime(source) > getmtime(py):
+                    print("Updating resource file: {}".format(py))
+                    subprocess.check_call(["pyrcc4", "-py3", "-o", py, sources[0]])
+                    break
+    
+    # Now we monkey-path the relevant commands to update i18n & resources on demand
+    sdist_run = sdist.sdist.run
+    def wrapped_sdist_run(self):
+        updateTranslations()
+        updateResources()
+        sdist_run(self)
+    sdist.sdist.run = wrapped_sdist_run
+    
+    install_run = install.install.run
+    def wrapped_install_run(self):
+        updateTranslations()
+        updateResources()
+        install_run(self) 
+    install.install.run = wrapped_install_run
+    
+    develop_run = develop.develop.run
+    def wrapped_develop_run(self):
+        updateTranslations()
+        updateResources()
+        develop_run(self)
+    develop.develop.run = wrapped_develop_run
+    
+    test_run = test.test.run
+    def wrapped_test_run(self):
+        updateTranslations()
+        updateResources()
+        test_run(self)
+    test.test.run = wrapped_test_run
 
 setup(name='omg',
-      version='0.3-currentgit',
+      version='0.3',
       description='OMG music GUI',
       author='Martin Altmayer, Michael Helmling',
       author_email='{altmayer,helmling}@mathematik.uni-kl.de',
@@ -109,7 +110,7 @@ setup(name='omg',
       license='GPL3',
       packages=find_packages(),
       include_package_data=True,
-      py_modules=['mpd', 'distribute_setup'],
+      #py_modules=['distribute_setup'],
       entry_points = {
           'gui_scripts' : ['omg = omg.application:run',
                            'omgsetup = omg.install:run',
