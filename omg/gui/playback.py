c85d280660b979c4fe54ab26c85583d45697c31b
Martin Altmayer <martin.altmayer@web.de>
2013-07-03 22:39:17 +0200

    Added a popup menu to the volume button. Improved other playback buttons.

--- omg/gui/playback.py
+++ omg/gui/playback.py
@@ -45,38 +45,36 @@ class PlaybackWidget(dockwidget.DockWidget):
         elif len(player.profileCategory.profiles()) > 0:
             backend = player.profileCategory.profiles()[0]
         else: backend = None
-        topLayout = QtGui.QHBoxLayout()       
         
-        standardIcon = QtGui.qApp.style().standardIcon
-        self.previousButton = QtGui.QPushButton(standardIcon(QtGui.QStyle.SP_MediaSkipBackward), '', self)
-        self.ppButton = PlayPauseButton(self)
-        self.stopButton = QtGui.QPushButton(standardIcon(QtGui.QStyle.SP_MediaStop), '', self)
-        self.nextButton = QtGui.QPushButton(standardIcon(QtGui.QStyle.SP_MediaSkipForward), '', self)
-        self.ppButton.setIconSize(QtCore.QSize(10,16))
-        self.stopButton.setIconSize(QtCore.QSize(10,16))
-        self.previousButton.setIconSize(QtCore.QSize(16,16))
-        self.nextButton.setIconSize(QtCore.QSize(16,16))
-        self.volumeLabel = VolumeLabel(self)
-        policy = QtGui.QSizePolicy()
-        policy.setHorizontalPolicy(QtGui.QSizePolicy.Fixed)
-        for w in (self.previousButton, self.ppButton,
-                       self.stopButton, self.nextButton, self.volumeLabel):
-            w.setSizePolicy(policy)
-            
+        topLayout = QtGui.QHBoxLayout()    
         self.titleLabel = QtGui.QLabel(self)
         self.titleLabel.setTextFormat(Qt.AutoText)
         self.titleLabel.setWordWrap(True)
-        topLayout.addWidget(self.titleLabel)
-        self.seekSlider = PlaybackSlider(Qt.Horizontal, self)
-        self.seekSlider.setRange(0, 1000)
+        topLayout.addWidget(self.titleLabel)   
         
+        toolBar = QtGui.QToolBar()
+        toolBar.setIconSize(QtCore.QSize(16, 16))
+        standardIcon = QtGui.qApp.style().standardIcon
+        self.previousButton = QtGui.QToolButton()
+        self.previousButton.setIcon(standardIcon(QtGui.QStyle.SP_MediaSkipBackward))
+        self.ppButton = PlayPauseButton(self)
+        self.stopButton = QtGui.QToolButton()
+        self.stopButton.setIcon(standardIcon(QtGui.QStyle.SP_MediaStop))
+        self.nextButton = QtGui.QToolButton()
+        self.nextButton.setIcon(standardIcon(QtGui.QStyle.SP_MediaSkipForward))
+        self.volumeButton = VolumeButton()
+        toolBar.addWidget(self.previousButton)
+        toolBar.addWidget(self.ppButton)
+        toolBar.addWidget(self.stopButton)
+        toolBar.addWidget(self.nextButton)
+        topLayout.addWidget(toolBar)
+        # Keep the volume button outside the toolbar to allow for a slightly bigger icon
+        topLayout.addWidget(self.volumeButton)
+            
         bottomLayout = QtGui.QHBoxLayout()
         self.seekLabel = QtGui.QLabel("", self)
-        topLayout.addWidget(self.previousButton)
-        topLayout.addWidget(self.ppButton)
-        topLayout.addWidget(self.stopButton)
-        topLayout.addWidget(self.nextButton)
-        topLayout.addWidget(self.volumeLabel)
+        self.seekSlider = PlaybackSlider(Qt.Horizontal, self)
+        self.seekSlider.setRange(0, 1000)
         bottomLayout.addWidget(self.seekSlider)
         bottomLayout.addWidget(self.seekLabel)
         mainLayout = QtGui.QVBoxLayout(widget)
@@ -134,7 +132,7 @@ class PlaybackWidget(dockwidget.DockWidget):
     def handleConnectionChange(self, state):
         """Update GUI elements when the connection state has changed."""
         for item in self.previousButton, self.ppButton, self.stopButton, \
-                    self.nextButton, self.seekSlider, self.seekLabel, self.volumeLabel:
+                    self.nextButton, self.seekSlider, self.seekLabel, self.volumeButton:
             item.setEnabled(state is player.CONNECTED)
         if state == player.CONNECTING:
             self.titleLabel.setText(self.tr("connecting..."))
@@ -143,7 +141,7 @@ class PlaybackWidget(dockwidget.DockWidget):
         else:
             self.updateTitleLabel(self.backend.current())
             self.handleStateChange(self.backend.state())
-            self.volumeLabel.setVolume(self.backend.volume())
+            self.volumeButton.setVolume(self.backend.volume())
     
     def handlePlaylistChange(self, *args):
         """Enable or disable play and stop buttons when the playlist becomes empty / is filled."""
@@ -152,13 +150,13 @@ class PlaybackWidget(dockwidget.DockWidget):
         self.stopButton.setEnabled(not playlistEmpty)
     
     signals = [ ("self.backend.elapsedChanged", "self.updateSlider"),
-                ("self.backend.volumeChanged", "self.volumeLabel.setVolume"),
+                ("self.backend.volumeChanged", "self.volumeButton.setVolume"),
                 ("self.backend.stateChanged", "self.handleStateChange"),
                 ("self.backend.currentChanged", "self.updateTitleLabel"),
                 ("self.backend.connectionStateChanged", "self.handleConnectionChange"),
                 ("self.backend.playlist.rowsInserted", "self.handlePlaylistChange"),
                 ("self.backend.playlist.rowsRemoved", "self.handlePlaylistChange"),
-                ("self.volumeLabel.volumeRequested", "self.backend.setVolume"),
+                ("self.volumeButton.volumeChanged", "self.backend.setVolume"),
                 ("self.ppButton.stateChanged", "self.backend.setState"),
                 ("self.stopButton.clicked", "self.backend.stop"),
                 ("self.seekSlider.sliderMoved", "self.backend.setElapsed"),
@@ -202,15 +200,15 @@ mainwindow.addWidgetData(data)
 
 
 class PlaybackSlider(QtGui.QSlider):
-    
     def mouseReleaseEvent(self, event):
         if not self.isSliderDown():
             val = QtGui.QStyle.sliderValueFromPosition(self.minimum(),
                     self.maximum(), event.x(), self.width())
             self.sliderMoved.emit(val)
         return super().mouseReleaseEvent(event)
-        
-class PlayPauseButton(QtGui.QPushButton):
+
+
+class PlayPauseButton(QtGui.QToolButton):
     """Special button with two states. Depending on the state different signals (play and pause)
     are emitted when the button is clicked and the button shows different icons."""
     
@@ -221,9 +219,10 @@ class PlayPauseButton(QtGui.QPushButton):
     pauseIcon = QtGui.qApp.style().standardIcon(QtGui.QStyle.SP_MediaPause)
     stateChanged = QtCore.pyqtSignal(int)
     
-    def __init__(self,parent):
+    def __init__(self, parent=None):
         """Initialize this button with the given parent. The button will be in pause-state."""
-        QtGui.QPushButton.__init__(self,self.playIcon,'',parent)
+        super().__init__(parent)
+        self.setIcon(self.playIcon)
         self.playing = False
         self.clicked.connect(lambda : self.pause.emit() if self.playing else self.play.emit() )
         self.pause.connect(lambda : self.stateChanged.emit(player.PAUSE))
@@ -234,72 +233,140 @@ class PlayPauseButton(QtGui.QPushButton):
         if playing != self.playing:
             self.playing = playing
             self.setIcon(self.pauseIcon if playing else self.playIcon)
-            
-class VolumeLabel(QtGui.QLabel):
-    """Special label displaying an icon that visualizes the current volume setting and
-    emits signals when clicked (which means mute/unmute) or mouse-scrolled (to change
-    volume)."""
-   
-    volumeRequested = QtCore.pyqtSignal(int)
+        
     
-    mutedIcon = utils.getPixmap('volume_muted.png')
-    lowIcon = utils.getPixmap('volume_low.png')
-    mediumIcon = utils.getPixmap('volume_medium.png')
-    highIcon = utils.getPixmap('volume_high.png')
+class VolumeButton(QtGui.QToolButton):
+    """Button displaying an icon that visualizes the current volume. When clicked it opens a popup menu
+    that allows to change the volume. Alternatively the volume can be changed using the mouse-wheel.
+    The middle mouse button can be used to mute/unmute.
+    """
+    # Inspired by the VolumePopupButton from Amarok 2.7.1
+    volumeChanged = QtCore.pyqtSignal(int)
+    
+    mutedIcon = utils.getIcon('volume_muted.png')
+    lowIcon = utils.getIcon('volume_low.png')
+    mediumIcon = utils.getIcon('volume_medium.png')
+    highIcon = utils.getIcon('volume_high.png')
     
     def __init__(self, parent=None):
-        """Initialize this label with the given parent."""
-        QtGui.QLabel.__init__(self, parent)
+        super().__init__(parent)
+        self.setIconSize(QtCore.QSize(24, 24))
+        self.setContentsMargins(0,0,0,0)
+        self.setAutoRaise(True)
+        
+        self.popup = QtGui.QWidget()
+        layout = QtGui.QVBoxLayout(self.popup)
+        layout.setContentsMargins(1,1,1,1)
+        layout.setSpacing(0)
+        self.volumeLabel = QtGui.QLabel()
+        # Make sure the menu is wide enough to hold all possible values
+        self.volumeLabel.setText(self.tr("{}%").format(100))
+        self.volumeLabel.setMinimumWidth(self.volumeLabel.sizeHint().width())
+        self.volumeLabel.setAlignment(Qt.AlignHCenter)
+        layout.addWidget(self.volumeLabel, 0, Qt.AlignHCenter)
+        
+        self.volumeSlider = VolumeSlider()
+        self.volumeSlider.setFixedHeight(170)
+        self.volumeSlider.valueChanged.connect(self.setVolume)
+        layout.addWidget(self.volumeSlider, 0, Qt.AlignHCenter)
+        
+        muteButton = QtGui.QToolButton()
+        muteButton.setIcon(self.mutedIcon)
+        muteButton.setIconSize(QtCore.QSize(20, 20))
+        muteButton.clicked.connect(self.toggleMute)
+        layout.addWidget(muteButton, 0, Qt.AlignHCenter)
+        
+        self.menu = QtGui.QMenu()
+        action = QtGui.QWidgetAction(self)
+        action.setDefaultWidget(self.popup)
+        self.menu.addAction(action)
+        
+        # When the mouse-wheel is scrolled, the volume is changed only after a short delay.
+        self._emitTimer = QtCore.QTimer(self)
+        self._emitTimer.setSingleShot(True)
+        self._emitTimer.timeout.connect(self._emit)
+        self._newVolume = None
+        
         self.volume = -1
         self.lastVolume = 0
         self.setVolume(0)
-        self.emitTimer = QtCore.QTimer(self)
-        self.emitTimer.setSingleShot(True)
-        self.emitTimer.timeout.connect(self._emit)
-        self.changeRequest = None
-   
+        
     def setVolume(self, volume):
-        """Display the appropriate icon for the given volume."""
+        """Set the volume of this widget and emit volumeChanged."""
         if volume != self.volume:
-            self.setVisible(volume != -1)
-            self.setPixmap(self.volumeIcon(volume))
+            self.setEnabled(volume != -1)
+            self.setIcon(self.volumeIcon(volume))
             if volume == 0:
                 self.lastVolume = self.volume
             self.volume = volume
-            self.setToolTip(self.tr('{}%').format(volume))
-        
+            text = self.tr('{}%').format(volume) if volume >= 0 else ''
+            self.volumeLabel.setText(text)
+            self.setToolTip(text)
+            self.volumeSlider.setValue(volume)
+            if volume != -1:
+                self.volumeChanged.emit(volume)
+
     @staticmethod
     def volumeIcon(volume):
         """Maps the given volume to the appropriate icon."""
         if volume == 0:
-            return VolumeLabel.mutedIcon
+            return VolumeButton.mutedIcon
         elif volume <= 33:
-            return VolumeLabel.lowIcon
+            return VolumeButton.lowIcon
         elif volume <= 66:
-            return VolumeLabel.mediumIcon
-        return VolumeLabel.highIcon
-       
-    def mousePressEvent(self, event):
-        if self.volume == 0:
-            self.volumeRequested.emit(self.lastVolume)
-        else:
-            self.volumeRequested.emit(0)
-        event.accept()
-    
-    def _emit(self):
-        if self.changeRequest is not None:
-            self.volumeRequested.emit(self.changeRequest)
-            self.changeRequest = None
+            return VolumeButton.mediumIcon
+        return VolumeButton.highIcon
             
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            if self.menu.isVisible():
+                self.menu.hide()
+            else:
+                self.menu.exec_(self.mapToGlobal(QtCore.QPoint(0, self.height())))
+            event.accept()
+        elif event.button() == Qt.MidButton:
+            self.toggleMute()
+            event.accept()
+        super().mouseReleaseEvent(event)
+    
     def wheelEvent(self, event):
-        volume = self.changeRequest if self.changeRequest is not None else self.volume
-        req = volume + event.delta()//20
+        # When the mouse-wheel is scrolled, the volume is changed only after a short delay.
+        # _newVolume is used to store the requested value between several successive wheel-events.  
+        volume = self._newVolume if self._newVolume is not None else self.volume
+        req = volume + event.delta() // 20
         if req > 100:
             req = 100
         if req < 0:
             req = 0
-        self.changeRequest = req
-        self.emitTimer.start(25)
+        self._newVolume = req
+        self._emitTimer.start(25)
+    
+    def _emit(self):
+        if self._newVolume is not None:
+            self.setVolume(self._newVolume)
+            self._newVolume = None
+            
+    def toggleMute(self):
+        """Toggle volume between 0 and the last volume before muting."""
+        if self.volume == 0:
+            self.setVolume(self.lastVolume)
+        else: self.setVolume(0)
+        
+
+class VolumeSlider(QtGui.QSlider):
+    """Special slider used in the menu of VolumeButton."""
+    def __init__(self, parent=None):
+        super().__init__(Qt.Vertical, parent)
+        self.setMinimum(0)
+        self.setMaximum(100)
+        
+    def contextMenuEvent(self, event):
+        menu = QtGui.QMenu()
+        for v in [100, 80, 60, 40, 20, 0]:
+            menu.addAction(self.tr("{}%").format(v)).setData(v)
+        action = menu.exec_(self.mapToGlobal(event.pos()))
+        if action is not None:
+            self.setValue(action.data())
 
 
 class OptionDialog(dialogs.FancyPopup):
