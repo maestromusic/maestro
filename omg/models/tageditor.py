22ec7e841a8e9aedb767613eefb9769f84b64f7e
Michael Helmling <michaelhelmling@posteo.de>
2014-05-02 11:28:57 +0200

    remove a print message

--- omg/models/tageditor.py
+++ omg/models/tageditor.py
@@ -635,7 +635,6 @@ class TagEditorModel(QtCore.QObject):
         for tag in self.records.tags():
             if tag not in actualRecords:
                 for record in self.records[tag][:]:
-                    print('ha removing {}'.format(record))
                     self.records.removeRecord(record)
                 self.records.removeTag(tag)
                 changed = True
